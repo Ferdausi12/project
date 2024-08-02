525e116ac30fde8330ced71f6189245b0574ea28
Ferdausi <mstferdausi12@gmail.com>
2024-08-02 22:49:36 +0600

    second

--- car_sales_project/urls.py
+++ car_sales_project/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name = 'home'),
-    path('cars/', include('carlistings.urls')),
+    path('', include('carlistings.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- carlistings/views.py
+++ carlistings/views.py
@@ -4,7 +4,8 @@ from .models import Car
 from .forms import CarForm
 
 # Create your views here.
-
+def home(request):
+    return render(request,'index.html')
 
 def car_list(request):
     cars = Car.objects.all()
